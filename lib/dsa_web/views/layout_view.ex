7737d88c51b5f14dd44f31db459b1fc8eb6fe8f4
fuchsberger <admin@fuchsberger.us>
2020-07-04 13:16:23 -0400

    bootstrap integrated

--- lib/dsa_web/views/layout_view.ex
+++ lib/dsa_web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule DsaWeb.LayoutView do
   use DsaWeb, :view
+
+  def flash(conn, type) do
+    case get_flash(conn, type) do
+      nil -> nil
+      msg ->
+        color = if type == :error, do: "danger", else: Atom.to_string(type)
+
+        ~E"""
+          <div class='alert alert-<%= color %> alert-dismissible fade show' role='alert'>
+            <%= msg %>
+            <button type='button' class='close' data-dismiss='alert' aria-lable='close'>
+              <span area-hidden='true'>&times;</span>
+            </button>
+          </div>
+        """
+    end
+  end
 end
