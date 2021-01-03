5864f88fb217500952889249352a01f0941c8e73
fuchsberger <admin@fuchsberger.us>
2021-01-03 01:03:35 -0500

    fixes

--- lib/dsa/event/log.ex
+++ lib/dsa/event/log.ex
@@ -5,6 +5,7 @@ defmodule Dsa.Event.Log do
   @fields ~w(type x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12)a
 
   schema "logs" do
+
     Enum.each(@fields, & field(&1, :integer))
 
     belongs_to :character, Dsa.Accounts.Character
