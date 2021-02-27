dbd855807454e9c35abaecdd048735575af5ad3b
fuchsberger <admin@fuchsberger.us>
2021-02-27 17:41:19 -0500

    added turbo to make app feel more like a SPA

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 import "../css/app.css"
 import "phoenix_html"
+import * as Turbo from "@hotwired/turbo"
 
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
@@ -14,43 +15,45 @@ let liveSocket = new LiveSocket("/live", Socket, {
 liveSocket.connect()
 
 
-// Select mobile button
-let mobile_button = document.getElementById("mobile-button")
+document.addEventListener("turbo:load", function() {
 
-// Add a click event on it
-mobile_button.addEventListener('click', e => {
-
-  // Toggle the classes in the mobile menu button
-  for(let svg of mobile_button.getElementsByTagName("svg")){
-    svg.classList.toggle("block")
-    svg.classList.toggle("hidden")
-  }
-
-  let menu = document.getElementById("mobile-menu")
-  menu.classList.toggle("hidden")
-  menu.classList.toggle("block")
-})
-
-
-// Enable Log Button
-let el = document.getElementById("log-button")
-
-if (el !== null) {
+  // Select mobile button
+  let mobile_button = document.getElementById("mobile-button")
 
   // Add a click event on it
-  el.addEventListener('click', () => {
+  mobile_button.addEventListener('click', e => {
 
     // Toggle the classes in the mobile menu button
-    el.classList.toggle("active")
-
-    for(let elm of el.children){
-      elm.classList.toggle("block")
-      elm.classList.toggle("hidden")
+    for(let svg of mobile_button.getElementsByTagName("svg")){
+      svg.classList.toggle("block")
+      svg.classList.toggle("hidden")
     }
-  })
-}
 
-// Enable Notification dismissal
-(document.querySelectorAll('button.dismiss') || []).forEach(elm =>
-  elm.addEventListener('click', () => elm.parentNode.parentNode.removeChild(elm.parentNode))
-);
+    let menu = document.getElementById("mobile-menu")
+    menu.classList.toggle("hidden")
+    menu.classList.toggle("block")
+  })
+
+  // Enable Log Button
+  let el = document.getElementById("log-button")
+
+  if (el !== null) {
+
+    // Add a click event on it
+    el.addEventListener('click', () => {
+
+      // Toggle the classes in the mobile menu button
+      el.classList.toggle("active")
+
+      for(let elm of el.children){
+        elm.classList.toggle("block")
+        elm.classList.toggle("hidden")
+      }
+    })
+  }
+
+  // Enable Notification dismissal
+  (document.querySelectorAll('button.dismiss') || []).forEach(elm =>
+    elm.addEventListener('click', () => elm.parentNode.parentNode.removeChild(elm.parentNode))
+  )
+})
