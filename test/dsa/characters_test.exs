c82d352ecf65564ce4809161ff1cab02af15c864
fuchsberger <admin@fuchsberger.us>
2021-03-17 22:53:26 -0400

    finished context tests of combat sets

--- test/dsa/characters_test.exs
+++ test/dsa/characters_test.exs
@@ -1,79 +1,78 @@
 defmodule Dsa.CharactersTest do
   use Dsa.DataCase
 
-  alias Dsa.Characters
-
   describe "combat_sets" do
+    alias Dsa.Characters
     alias Dsa.Characters.CombatSet
 
-    @valid_attrs %{at: 42, aw: 42, base_ini: 42, be: 42, gs: 42, pa: 42, rs: 42, tp_bonus: 42, tp_dice: 42}
-    @update_attrs %{at: 43, aw: 43, base_ini: 43, be: 43, gs: 43, pa: 43, rs: 43, tp_bonus: 43, tp_dice: 43}
-    @invalid_attrs %{at: nil, aw: nil, base_ini: nil, be: nil, gs: nil, pa: nil, rs: nil, tp_bonus: nil, tp_dice: nil}
+    @valid_attrs %{name: "TestSet", at: 10, pa: 10, tp_bonus: 0, tp_dice: 1, tp_type: 6}
+    @update_attrs %{name: "TestSet2", at: 11, pa: 11, tp_bonus: 1, tp_dice: 2, tp_type: 7}
+    @invalid_attrs %{name: nil, at: nil, pa: nil, tp_bonus: nil, tp_dice: nil, tp_type: nil}
 
-    def combat_set_fixture(attrs \\ %{}) do
-      {:ok, combat_set} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Characters.create_combat_set()
+    setup do
+      user = user_fixture()
+      character = character_fixture(user)
+      {:ok, character: character}
+    end
 
+    def combat_set_fixture(character, attrs \\ %{}) do
+      {:ok, combat_set} = Characters.create_combat_set(character, Enum.into(attrs, @valid_attrs))
       combat_set
     end
 
-    test "list_combat_sets/0 returns all combat_sets" do
-      combat_set = combat_set_fixture()
-      assert Characters.list_combat_sets() == [combat_set]
+    test "list_combat_sets/1 returns all combat_sets", %{character: character} do
+      %CombatSet{id: id1} = combat_set_fixture(character)
+      assert [%CombatSet{id: ^id1}] = Characters.list_combat_sets(character)
+      %CombatSet{id: id2} = combat_set_fixture(character)
+      assert [%CombatSet{id: ^id1}, %CombatSet{id: ^id2}] = Characters.list_combat_sets(character)
     end
 
-    test "get_combat_set!/1 returns the combat_set with given id" do
-      combat_set = combat_set_fixture()
-      assert Characters.get_combat_set!(combat_set.id) == combat_set
+    test "get_combat_set!/2 returns the combat_set with given id", %{character: character} do
+      %CombatSet{id: id} = combat_set_fixture(character)
+      assert %CombatSet{id: ^id} = Characters.get_combat_set!(character, id)
     end
 
-    test "create_combat_set/1 with valid data creates a combat_set" do
-      assert {:ok, %CombatSet{} = combat_set} = Characters.create_combat_set(@valid_attrs)
-      assert combat_set.at == 42
-      assert combat_set.aw == 42
-      assert combat_set.base_ini == 42
-      assert combat_set.be == 42
-      assert combat_set.gs == 42
-      assert combat_set.pa == 42
-      assert combat_set.rs == 42
-      assert combat_set.tp_bonus == 42
-      assert combat_set.tp_dice == 42
+    test "create_combat_set/2 with valid data creates a combat_set", %{character: c} do
+      assert {:ok, %CombatSet{} = combat_set} = Characters.create_combat_set(c, @valid_attrs)
+      assert combat_set.name == "TestSet"
+      assert combat_set.at == 10
+      assert combat_set.pa == 10
+      assert combat_set.tp_bonus == 0
+      assert combat_set.tp_dice == 1
+      assert combat_set.tp_type == 6
     end
 
-    test "create_combat_set/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Characters.create_combat_set(@invalid_attrs)
+    test "create_combat_set/2 with invalid data returns error changeset", %{character: c} do
+      assert {:error, %Ecto.Changeset{}} = Characters.create_combat_set(c, @invalid_attrs)
     end
 
-    test "update_combat_set/2 with valid data updates the combat_set" do
-      combat_set = combat_set_fixture()
-      assert {:ok, %CombatSet{} = combat_set} = Characters.update_combat_set(combat_set, @update_attrs)
-      assert combat_set.at == 43
-      assert combat_set.aw == 43
-      assert combat_set.base_ini == 43
-      assert combat_set.be == 43
-      assert combat_set.gs == 43
-      assert combat_set.pa == 43
-      assert combat_set.rs == 43
-      assert combat_set.tp_bonus == 43
-      assert combat_set.tp_dice == 43
+    test "update_combat_set/2 with valid data updates the combat_set", %{character: c} do
+      combat_set = combat_set_fixture(c)
+      assert {:ok, %CombatSet{} = combat_set} =
+        Characters.update_combat_set(combat_set, @update_attrs)
+      assert combat_set.name == "TestSet2"
+      assert combat_set.at == 11
+      assert combat_set.pa == 11
+      assert combat_set.tp_bonus == 1
+      assert combat_set.tp_dice == 2
+      assert combat_set.tp_type == 7
     end
 
-    test "update_combat_set/2 with invalid data returns error changeset" do
-      combat_set = combat_set_fixture()
-      assert {:error, %Ecto.Changeset{}} = Characters.update_combat_set(combat_set, @invalid_attrs)
-      assert combat_set == Characters.get_combat_set!(combat_set.id)
+    test "update_combat_set/2 with invalid data returns error changeset", %{character: c} do
+      %CombatSet{id: id} = combat_set = combat_set_fixture(c)
+      assert {:error, %Ecto.Changeset{}} =
+        Characters.update_combat_set(combat_set, @invalid_attrs)
+      assert %CombatSet{id: ^id} = Characters.get_combat_set!(c, id)
     end
 
-    test "delete_combat_set/1 deletes the combat_set" do
-      combat_set = combat_set_fixture()
+    test "delete_combat_set/1 deletes the combat_set", %{character: character} do
+      combat_set = combat_set_fixture(character)
       assert {:ok, %CombatSet{}} = Characters.delete_combat_set(combat_set)
-      assert_raise Ecto.NoResultsError, fn -> Characters.get_combat_set!(combat_set.id) end
+      assert Characters.list_combat_sets(character) == []
     end
 
-    test "change_combat_set/1 returns a combat_set changeset" do
-      combat_set = combat_set_fixture()
+    test "change_combat_set/1 returns a combat_set changeset", %{character: character} do
+      combat_set = combat_set_fixture(character)
       assert %Ecto.Changeset{} = Characters.change_combat_set(combat_set)
     end
   end
--- test/dsa_web/controllers/combat_set_controller_test.exs
+++ test/dsa_web/controllers/combat_set_controller_test.exs
@@ -1,88 +1,23 @@
 defmodule DsaWeb.CombatSetControllerTest do
   use DsaWeb.ConnCase
 
-  alias Dsa.Characters
+  describe "combat sets" do
+    alias Dsa.Characters
+    alias Dsa.Characters.CombatSet
 
-  @create_attrs %{at: 10, aw: 6, base_ini: 10, be: 0, gs: 6, pa: 10, rs: 0, tp_bonus: 0, tp_dice: 1, tp_type: 6}
-  @update_attrs %{at: 11, aw: 7, base_ini: 11, be: 1, gs: 7, pa: 11, rs: 1, tp_bonus: 1, tp_dice: 2, tp_type: 7}
-  @invalid_attrs %{at: nil, aw: nil, base_ini: nil, be: nil, gs: nil, pa: nil, rs: nil, tp_bonus: nil, tp_dice: nil}
+    @valid_attrs %{name: "TestSet", at: 10, pa: 10, tp_bonus: 0, tp_dice: 1, tp_type: 6}
+    @invalid_attrs %{name: nil, at: nil, pa: nil, tp_bonus: nil, tp_dice: nil, tp_type: nil}
 
-  # def fixture(:combat_set) do
-  #   {:ok, combat_set} = Characters.create_combat_set(@create_attrs)
-  #   combat_set
-  # end
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      character = character_fixture(user)
+      {:ok, conn: authenticate_user(assign(conn, :current_user, user)), character: character}
+    end
 
-  # describe "index" do
-  #   test "lists all combat_sets", %{conn: conn} do
-  #     conn = get(conn, Routes.character_combat_set_path(conn, :index, @character))
-  #     assert html_response(conn, 200) =~ "Listing Combat sets"
-  #   end
-  # end
+    @tag login_as: "Max"
+    test "list_combat_sets/1 returns all character combat sets", %{conn: conn, character: character} do
 
-  # describe "new combat_set" do
-  #   test "renders form", %{conn: conn} do
-  #     conn = get(conn, Routes.character_combat_set_path(conn, :new))
-  #     assert html_response(conn, 200) =~ "New Combat set"
-  #   end
-  # end
+    end
 
-  # describe "create combat_set" do
-  #   test "redirects to show when data is valid", %{conn: conn} do
-  #     conn = post(conn, Routes.character_combat_set_path(conn, :create), combat_set: @create_attrs)
-
-  #     assert %{id: id} = redirected_params(conn)
-  #     assert redirected_to(conn) == Routes.character_combat_set_path(conn, :show, id)
-
-  #     conn = get(conn, Routes.character_combat_set_path(conn, :show, id))
-  #     assert html_response(conn, 200) =~ "Show Combat set"
-  #   end
-
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post(conn, Routes.character_combat_set_path(conn, :create), combat_set: @invalid_attrs)
-  #     assert html_response(conn, 200) =~ "New Combat set"
-  #   end
-  # end
-
-  # describe "edit combat_set" do
-  #   setup [:create_combat_set]
-
-  #   test "renders form for editing chosen combat_set", %{conn: conn, combat_set: combat_set} do
-  #     conn = get(conn, Routes.character_combat_set_path(conn, :edit, combat_set))
-  #     assert html_response(conn, 200) =~ "Edit Combat set"
-  #   end
-  # end
-
-  # describe "update combat_set" do
-  #   setup [:create_combat_set]
-
-  #   test "redirects when data is valid", %{conn: conn, combat_set: combat_set} do
-  #     conn = put(conn, Routes.character_combat_set_path(conn, :update, combat_set), combat_set: @update_attrs)
-  #     assert redirected_to(conn) == Routes.character_combat_set_path(conn, :show, combat_set)
-
-  #     conn = get(conn, Routes.character_combat_set_path(conn, :show, combat_set))
-  #     assert html_response(conn, 200)
-  #   end
-
-  #   test "renders errors when data is invalid", %{conn: conn, combat_set: combat_set} do
-  #     conn = put(conn, Routes.character_combat_set_path(conn, :update, combat_set), combat_set: @invalid_attrs)
-  #     assert html_response(conn, 200) =~ "Edit Combat set"
-  #   end
-  # end
-
-  # describe "delete combat_set" do
-  #   setup [:create_combat_set]
-
-  #   test "deletes chosen combat_set", %{conn: conn, combat_set: combat_set} do
-  #     conn = delete(conn, Routes.character_combat_set_path(conn, :delete, combat_set))
-  #     assert redirected_to(conn) == Routes.character_combat_set_path(conn, :index)
-  #     assert_error_sent 404, fn ->
-  #       get(conn, Routes.character_combat_set_path(conn, :show, combat_set))
-  #     end
-  #   end
-  # end
-
-  # defp create_combat_set(_) do
-  #   combat_set = fixture(:combat_set)
-  #   %{combat_set: combat_set}
-  # end
+  end
 end
