8690052b2d00f478e30eb6399bc134b8c156e0db
Benjamin Hiltpolt <benjamin.hiltpolt@xing.com>
2021-03-18 18:59:33 +0100

    Add roll

--- priv/repo/migrations/20210317180502_add_main_log_table.exs
+++ priv/repo/migrations/20210317180502_add_main_log_table.exs
@@ -7,6 +7,7 @@ defmodule Dsa.Repo.Migrations.AddMainLogTable do
       add :character_name, :string
       add :left, :string
       add :right, :string
+      add :roll, :integer
       add :character_id, references(:characters)
       add :group_id, references(:groups, on_delete: :delete_all)
       timestamps()
