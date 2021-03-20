443f59e7be3dd19044a078d1bfd930d6fa60885b
fuchsberger <admin@fuchsberger.us>
2021-03-19 21:36:49 -0400

    improved postcss handling of assets

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     '../lib/**/*.ex',
@@ -5,9 +7,19 @@ module.exports = {
     '../lib/**/*.eex',
     './js/**/*.js'
   ],
-  theme: {},
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo
+    }
+  },
   variants: {},
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
+  plugins: []
 };
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -36,11 +36,12 @@ module.exports = (env, options) => ({
         use: { loader: 'babel-loader' }
       },
       {
-        test: /\.[s]?css$/,
+        test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,  // extract CSS into separate file
           'css-loader',                 // translates CSS into CommonJS
           'postcss-loader'              // CSS postprocessing
+
         ]
       },
       {
