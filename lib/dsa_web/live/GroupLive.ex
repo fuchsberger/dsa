925c77ac641c72717a4ae3961d5d3e6abb621d26
fuchsberger <admin@fuchsberger.us>
2021-03-21 11:36:12 -0400

    prepared group live view

---
+++ lib/dsa_web/live/GroupLive.ex
@@ -0,0 +1,9 @@
+defmodule DsaWeb.GroupLive do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~L"""
+    Test
+    """
+  end
+end
--- lib/dsa_web/router.ex
+++ lib/dsa_web/router.ex
@@ -8,7 +8,7 @@ defmodule DsaWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash
+    plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :put_root_layout, {DsaWeb.LayoutView, :root}
