ea2368dbd44b19de9964559507fdc76b41ce2f72
Benjamin Hiltpolt <benjamin.hiltpolt@xing.com>
2021-04-03 19:54:32 +0200

    Add a bit of data

--- lib/dsa/data/dice_table_seeds.ex
+++ lib/dsa/data/dice_table_seeds.ex
@@ -81,6 +81,77 @@ defmodule Dsa.Data.DiceTableSeeds do
         "12 Schwer verletzt",
         "Ein schwerer Eigentreffer. Der Schaden der Waffe wird unter Einbeziehung des Schadensbonus ausgewürfelt und dann verdoppelt."
       ]
+    ],
+    "Verteidigung-Patzertabelle" => [
+      [
+        "2 Schild zerstört",
+        "Der Schild ist unwiederbringlich zerstört. Bei unzerstörbaren Schilden wird das Ergebnis wie bei 5 behandelt."
+      ],
+      [
+        "3 Schild schwer beschädigt",
+        "Der Schild ist nicht mehr verwendbar, bis er repariert wird. Bei unzerstörbaren Schilden wird das Ergebnis wie bei 5 behandelt."
+      ],
+      [
+        "4 Schild beschädigt",
+        "Der Schild ist beschädigt worden. Alle Proben auf Attacke und Parade sind um 2 erschwert, bis er repariert wird. Bei unzerstörbaren Schilden wird das Ergebnis wie bei 5 behandelt. "
+      ],
+      ["5 Schild verloren", "Der Schild ist zu Boden gefallen. "],
+      [
+        "6 Schild stecken geblieben",
+        "Der Schild des Helden ist in einem Baum, einer Holzwand, dem Boden oder Ähnlichem stecken geblieben. Um ihn zu befreien, ist 1 Aktion und eine um 1 erschwerte Probe auf Kraft - akt (Ziehen & Zerren) notwendig. "
+      ],
+      [
+        "7 Sturz",
+        "Der Held stolpert und stürzt, wenn ihm nicht eine um 2 erschwerte Probe auf Körperbeherrschung (Balance) gelingt. Sollte er das nicht schaffen, erhält er den Status Liegend (siehe Seite 34). "
+      ],
+      ["8 Stolpern", "Der Held stolpert, seine nächste Handlung ist um 2 erschwert. "],
+      ["9 Fuß", "verdreht Der Held erhält für 3 Kampfrunden eine Stufe Schmerz . "],
+      [
+        "10 Beule",
+        "Der Held hat sich im Eifer des Gefechts den Kopf gestoßen. Er erhält eine Stufe Betäubung für eine Stunde. "
+      ],
+      [
+        "11 Selbst verletzt",
+        "Der Held hat sich selbst verletzt und er - leidet Schaden. Der Schaden seiner Waffe wird unter Einbeziehung des Schadens - bonus ausgewürfelt. Bei unbewaffneten Kämpfern wird 1W6 TP angenommen. "
+      ],
+      [
+        "12 Selbst schwer verletzt",
+        "Ein schwerer Eigentreffer des Helden. Der Schaden seiner Waffe wird unter Einbeziehung des Schadensbonus ausgewürfelt und dann verdoppelt. Bei unbewaffneten Kämpfern wird 1W6 TP angenommen."
+      ]
+    ],
+    "Zauber-Patzertabelle" => [
+      [
+        "2",
+        "Der Geist des Zauberers tauscht für 1W6 Tage den Körper mit dem nächsten Lebewesen in seiner Nähe, das größer ist als eine Ratte. "
+      ],
+      ["3", "Der Zauberer wird für 1W6 Tage von einem Mindergeist verfolgt. "],
+      ["4", "Das Ziel des Zaubers ändert sich zufällig nach Wahl des Meisters. "],
+      [
+        "5",
+        "Der Zauberer verwandelt sich für 1W6 Stunden in ein Kleintier nach Wahl des Spielleiters. "
+      ],
+      [
+        "6",
+        "Die gesamte Astralkraft verlässt den Körper des Zauberers und manifestiert sich als Regen aus bunten Funken, illusionären Schmetterlingen und kleinen Regenbögen. Die verlorenen Astralpunkte können regulär regeneriert werden. "
+      ],
+      [
+        "7",
+        "Der Zauberer verliert für 1W6 Tage die Fähigkeit, auf seinen AE-Vorrat zuzugreifen. "
+      ],
+      [
+        "8",
+        "Der Zauberer hat für die nächsten 1W6 Tage pochende Kopfschmerzen. Er leidet während dieser Zeit unter einer Stufe des Zustands Betäubung. "
+      ],
+      [
+        "9",
+        "Die gesamte Astralkraft verlässt den Körper des Zauberers und springt auf das nächste magisch begabte Ziel über. Das Ziel erhält die gesamten Astralpunkte des Zauberers, auch über sein natürliches Maximum an Astralenergie hinaus."
+      ],
+      ["10", "Der Zauberer verliert für 1W6 Tage seine Befähigung zu sprechen. "],
+      [
+        "11",
+        "Haar und Bart des Zauberers färben sich bunt, wachsen später jedoch in der ursprünglichen Farbe nach. "
+      ],
+      ["12", "Eine ätherische Stimme erklingt aus dem Nichts und sagt \„Tu das nie wieder!\“"]
     ]
   }
 
@@ -93,9 +164,9 @@ defmodule Dsa.Data.DiceTableSeeds do
       for entry <- entries do
         [result, description] = entry
 
-       IO.inspect(entry)
+        IO.inspect(entry)
 
-        {:ok, _ } =
+        {:ok, _} =
           Dsa.DiceTableEntries.create_dice_table_entry(
             %{"result" => result, "description" => description},
             table.id
