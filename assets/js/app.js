b56a0829ff5208d22b5035b3149c87990053f450
Alexander Fuchsberger <admin@fuchsberger.us>
2021-05-13 20:02:57 -0400

    login page finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,34 +24,6 @@ document.addEventListener("turbo:load", function() {
   // Connect if there are any LiveViews on the page
   liveSocket.connect()
 
-  // Enable mobile button
-  const mobile_button = document.getElementById("mobile-button")
-
-  mobile_button.addEventListener('click', () => {
-
-    mobile_button.classList.toggle("active")
-
-    for(let item of document.getElementsByClassName("menu-item")){
-      console.log(item)
-      item.classList.toggle("hidden")
-      item.classList.toggle("block")
-    }
-  })
-
-  // Enable log button
-  const log_button = document.getElementById("log-button")
-
-  if(log_button){
-    log_button.addEventListener('click', () => {
-      log_button.classList.toggle("active")
-
-      for(let elm of document.getElementById("main-wrapper").children){
-        elm.classList.toggle("block")
-        elm.classList.toggle("hidden")
-      }
-    })
-  }
-
   // Enable notification dismissal
   (document.querySelectorAll('button.dismiss') || []).forEach(elm =>
     elm.addEventListener('click', () => elm.parentNode.parentNode.removeChild(elm.parentNode))
