ad03051ffc6a6810f5ac5d2d429123932c1f9322
Alexander Fuchsberger <admin@fuchsberger.us>
2021-06-27 08:16:28 -0400

    updated dependencies

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Dsa.MixProject do
   defp deps do
     [
       {:algolia, "~> 0.8.0"},
-      {:bamboo, "~> 2.1.0"},
+      {:bamboo, "~> 2.2.0"},
       {:bcrypt_elixir, "~> 2.3"},
       {:ecto_psql_extras, "~> 0.6.5"},
       {:ecto_sql, "~> 3.6.1"},
@@ -44,10 +44,10 @@ defmodule Dsa.MixProject do
       {:gettext, "~> 0.18.2"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.5.9"},
-      {:phoenix_ecto, "~> 4.2.1"},
+      {:phoenix_ecto, "~> 4.3.0"},
       {:phoenix_html, "~> 2.14.3"},
       {:phoenix_live_dashboard, "~> 0.4"},
-      {:phoenix_live_reload, "~> 1.3.0", only: :dev},
+      {:phoenix_live_reload, "~> 1.3.2", only: :dev},
       {:phoenix_live_view, "~> 0.15.7"},
       {:phx_gen_auth, "~> 0.7.0", only: [:dev], runtime: false},
       {:plug_cowboy, "~> 2.5.0"},
